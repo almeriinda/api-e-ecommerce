8362103643070683a224f77e37bd03ee84843c31
almeriinda <melrylima.ce@gmail.com>
2023-10-20 15:34:58 -0300

    adicionando condicionais a rota createUser

--- controller/user.go
+++ controller/user.go
@@ -2,18 +2,27 @@ package controller
 
 import (
 	"api-e-commerce/models"
-	"encoding/json"
 	"github.com/gofiber/fiber/v2"
 )
 
 func CreateUser(c *fiber.Ctx) error {
-	body := c.Body()
-	user := models.User{}
-	json.Unmarshal(body, &user)
-	if len(user.PasswordHash) < 5 || len(user.PasswordHash) > 10 {
-		return c.Status(401).JSON("Tamanho da senha inválido")
+	var user models.User
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Erro ao analisar o corpo da solicitação",
+		})
 	}
-	user.CreateUser()
-	return c.Status(201).JSON("Usuário criado.")
-
+	if len(user.PasswordHash) < 5 || len(user.PasswordHash) > 10 {
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{
+			"message": "Tamanho da senha inválido (deve ter entre 5 e 10 caracteres)",
+		})
+	}
+	if err := user.CreateUser(); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Erro ao criar o usuário",
+		})
+	}
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "Usuário criado com sucesso",
+	})
 }
